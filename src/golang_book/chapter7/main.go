82407f4b3aa82a2aad80f09bd689abd7d1bf9b67
White-AK111 <formatmyc@gmail.com>
2021-07-02 00:34:50 +0300

    chap7

--- src/golang_book/chapter7/main.go
+++ src/golang_book/chapter7/main.go
@@ -2,188 +2,172 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
-//массивы, срезы, карты
-func main() {
+//Функции
 
-	var x [5]int
-	x[4] = 100
-	fmt.Println(x)
+//функция среднего
+func average(xs []float64) float64 {
+	//panic("Not Implemented")
 
-	var y [5]float64
-	y[0] = 98
-	y[1] = 93
-	y[2] = 77
-	y[3] = 82
-	y[4] = 83
-
-	var total float64 = 0
-	for i := 0; i < 5; i++ {
-		total += y[i]
+	total := 0.0
+	for _, v := range xs {
+		total += v
 	}
-	fmt.Println(total / 5)
+	return total / float64(len(xs))
+}
 
-	total = 0
-	for i := 0; i < len(y); i++ {
-		total += y[i]
+//функция возврата двух int
+func f() (int, int) {
+	return 5, 6
+}
+
+//функция суммирования
+func add(args ...int) int {
+	total := 0
+	for _, v := range args {
+		total += v
 	}
-	fmt.Println(total / float64(len(y)))
+	return total
+}
 
-	total = 0
-	for _, value := range y {
-		total += value
+//функция генерации чётных чисел
+func makeEvenGenerator() func() uint {
+	i := uint(0)
+	return func() (ret uint) {
+		ret = i
+		i += 2
+		return
 	}
-	fmt.Println(total / float64(len(y)))
+}
 
-	z := [5]float64{
-		100,
-		101,
-		102,
-		103,
-		104,
-	}
-	fmt.Println(z)
-
-	var slice1 []float64
-	fmt.Println(slice1)
-	slice2 := make([]float64, 5)
-	fmt.Println(slice2)
-	slice3 := make([]float64, 5, 10)
-	fmt.Println(slice3)
-	arr := [5]float64{1, 2, 3, 4, 5}
-	slice4 := arr[1:4]
-	fmt.Println(slice4)
-	fmt.Println(arr[1:])
-
-	sliceAppend1 := []int{1, 2, 3}
-	sliceAppend2 := append(sliceAppend1, 4, 5)
-	fmt.Println(sliceAppend1, sliceAppend2)
-
-	sliceCopy1 := []int{1, 2, 3}
-	sliceCopy2 := make([]int, 2)
-	copy(sliceCopy2, sliceCopy1)
-	fmt.Println(sliceCopy1, sliceCopy2)
-
-	mapSrtInt := make(map[string]int)
-	mapSrtInt["key"] = 10
-	fmt.Println(mapSrtInt["key"])
-
-	mapIntStr := make(map[int]string)
-	mapIntStr[1] = "one"
-	fmt.Println(mapIntStr[1])
-	delete(mapIntStr, 1)
-
-	elements := make(map[string]string)
-	elements["H"] = "Hydrogen"
-	elements["He"] = "Helium"
-	elements["Li"] = "Lithium"
-	elements["Be"] = "Beryllium"
-	elements["B"] = "Boron"
-	elements["C"] = "Carbon"
-	elements["N"] = "Nitrogen"
-	elements["O"] = "Oxygen"
-	elements["F"] = "Fluorine"
-	elements["Ne"] = "Neon"
-
-	fmt.Println(elements["Li"])
-
-	name, ok := elements["Un"]
-	fmt.Println(name, ok)
-
-	if name, ok := elements["Un"]; ok {
-		fmt.Println(name, ok)
+//функция рекурсии
+func factorial(x uint) uint {
+	if x == 0 {
+		return 1
 	}
 
-	elements2 := map[string]string{
-		"H":  "Hydrogen",
-		"He": "Helium",
-		"Li": "Lithium",
-		"Be": "Beryllium",
-		"B":  "Boron",
-		"C":  "Carbon",
-		"N":  "Nitrogen",
-		"O":  "Oxygen",
-		"F":  "Fluorine",
-		"Ne": "Neon",
+	return x * factorial(x-1)
+}
+
+//функция вывода 1st
+func first() {
+	fmt.Println("1st")
+}
+
+//функция вывода 2nd
+func second() {
+	fmt.Println("2nd")
+}
+
+//Функция sum принимает срез чисел и складывает их вместе. Как бы выглядела сигнатура этой функции?
+func sumSlice(slice []int) int {
+	total := 0
+	for _, num := range slice {
+		total += num
 	}
-	fmt.Println(elements2["B"])
+	return total
+}
 
-	elements3 := map[string]map[string]string{
-		"H": {
-			"name":  "Hydrogen",
-			"state": "gas",
-		},
-		"He": {
-			"name":  "Helium",
-			"state": "gas",
-		},
-		"Li": {
-			"name":  "Lithium",
-			"state": "solid",
-		},
-		"Be": {
-			"name":  "Beryllium",
-			"state": "solid",
-		},
-		"B": {
-			"name":  "Boron",
-			"state": "solid",
-		},
-		"C": {
-			"name":  "Carbon",
-			"state": "solid",
-		},
-		"N": {
-			"name":  "Nitrogen",
-			"state": "gas",
-		},
-		"O": {
-			"name":  "Oxygen",
-			"state": "gas",
-		},
-		"F": {
-			"name":  "Fluorine",
-			"state": "gas",
-		},
-		"Ne": {
-			"name":  "Neon",
-			"state": "gas",
-		},
+//Напишите функцию, которая принимает число, делит его пополам и возвращает true в случае, если исходное число чётное, и false, если нечетное. Например, half(1) должна вернуть (0, false), в то время как half(2) вернет (1, true).
+func half(num int) (byte, bool) {
+	if num%2 == 0 {
+		return 1, true
+	} else {
+		return 0, false
 	}
+}
 
-	if el, ok := elements3["O"]; ok {
-		fmt.Println(el["name"], el["state"])
-	}
-
-	//Как обратиться к четвертому элементу массива или среза?
-	arrEx1 := [5]int{1, 2, 3, 4, 5}
-	fmt.Println(arrEx1[3])
-	sliceEx1 := arrEx1[3:4]
-	fmt.Println(sliceEx1[0])
-
-	//Чему равна длина среза, созданного таким способом: make([]int, 3, 9)?
-	sliceEx2 := make([]int, 3, 9)
-	fmt.Println(len(sliceEx2))
-
-	//Дан массив: x := [6]string{"a","b","c","d","e","f"} что вернет вам x[2:5]?
-	arrEx3 := [6]string{"a", "b", "c", "d", "e", "f"}
-	fmt.Println(arrEx3[2:5])
-
-	//Напишите программу, которая находит самый наименьший элемент в этом списке:
-
-	arrEx4 := []int{
-		48, 96, 86, 68,
-		57, 82, 63, 70,
-		37, 34, 83, 27,
-		19, 97, 9, 17,
-	}
-
-	var minElem int = arrEx4[0]
-	for i := 1; i < len(arrEx4); i++ {
-		if arrEx4[i] < minElem {
-			minElem = arrEx4[i]
+//Напишите функцию с переменным числом параметров, которая находит наибольшее число в списке.
+func variableParams(nums ...int) int {
+	max := nums[0]
+	for _, n := range nums {
+		if n > max {
+			max = n
 		}
 	}
-	fmt.Println(minElem)
+	return max
+}
+
+//Используя в качестве примера функцию makeEvenGenerator напишите makeOddGenerator, генерирующую нечётные числа.
+func makeOddGenerator() func() uint {
+	i := uint(1)
+	return func() (ret uint) {
+		ret = i
+		i += 2
+		return
+	}
+}
+
+//Последовательность чисел Фибоначчи определяется как fib(0) = 0, fib(1) = 1, fib(n) = fib(n-1) + fib(n-2). Напишите рекурсивную функцию, находящую fib(n).
+func fibo(n int) int {
+	if n < 2 {
+		return n
+	} else {
+		return fibo(n-1) + fibo(n-2)
+	}
+}
+
+func main() {
+	xs := []float64{98, 93, 77, 82, 83}
+	fmt.Println(average(xs))
+
+	x, y := f()
+	fmt.Println(x + y)
+
+	fmt.Println(add(1, 2, 3))
+
+	xsl := []int{1, 2, 3}
+	fmt.Println(add(xsl...))
+
+	z := 0
+	increment := func() int {
+		z++
+		return z
+	}
+	fmt.Println(increment())
+	fmt.Println(increment())
+
+	nextEven := makeEvenGenerator()
+	fmt.Println(nextEven()) // 0
+	fmt.Println(nextEven()) // 2
+	fmt.Println(nextEven()) // 4
+
+	fmt.Println(factorial(5))
+
+	defer second()
+	first()
+
+	filename := `C:\Users\forma\OneDrive\Документы\Go\src\golang_book\chapter6\main.go`
+	f, _ := os.Open(filename)
+	defer f.Close()
+
+	//задание1
+	slice1 := []int{1, 2, 3, 4, 5}
+	fmt.Println(sumSlice(slice1))
+
+	//задание2
+	fmt.Println(half(0))
+	fmt.Println(half(4))
+	fmt.Println(half(5))
+	fmt.Println(half(10))
+
+	//задание3
+	fmt.Println(variableParams(1, 2, 3, 4, 5, 4, 8, 2))
+
+	//задание4
+	nextEven2 := makeOddGenerator()
+	fmt.Println(nextEven2()) // 1
+	fmt.Println(nextEven2()) // 3
+	fmt.Println(nextEven2()) // 5
+
+	//задание5
+	fmt.Println(fibo(22))
+
+	defer func() {
+		str := recover()
+		fmt.Println(str)
+	}()
+	panic("PANIC")
 }
