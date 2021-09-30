dc3937b3030035a8a774198fee378e92b5b50a6e
White-AK111 <formatmyc@gmail.com>
2021-09-30 08:40:12 -0700

    initial commit.

---
+++ src/GB_Go_Level2/Lesson3/main.go
@@ -0,0 +1,11 @@
+package Lesson3
+
+import (
+	_ "github.com/valyala/fasthttp"
+	_ "github.com/gorilla/websocket"
+)
+
+// main function is practice part of Lesson3
+func main() {
+ // pre-release
+}
