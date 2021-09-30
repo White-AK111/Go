1d2b00ad222638efcc26bd517781b3d2c619b12d
White-AK111 <formatmyc@gmail.com>
2021-09-30 08:46:36 -0700

    Lesson2 - done

--- src/GB_Go_Level2/Lesson3/main.go
+++ src/GB_Go_Level2/Lesson3/main.go
@@ -1,11 +1,11 @@
 package Lesson3
 
 import (
-	_ "github.com/valyala/fasthttp"
 	_ "github.com/gorilla/websocket"
+	_ "github.com/valyala/fasthttp"
 )
 
 // main function is practice part of Lesson3
 func main() {
- // pre-release
+	// release
 }
